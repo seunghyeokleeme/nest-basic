301dadd62cd59db992478a3bc6af232e2fc41fec
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-01-03 16:54:25 +0900

    4.2 Testing PATCH and DELETE movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -33,7 +33,7 @@ describe('AppController (e2e)', () => {
     it('GET', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -43,7 +43,18 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
-    it('DELETE', () => {
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE 404', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
   });
@@ -55,7 +66,14 @@ describe('AppController (e2e)', () => {
     it('GET 404', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
